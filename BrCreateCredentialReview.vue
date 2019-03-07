20bc13034eb5da7986af5544ee4bd51352b0bbbc
Dave Longley <dlongley@digitalbazaar.com>
2019-03-07 12:30:09 -0500

    Implement wizard and manual test.

---
+++ BrCreateCredentialReview.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="fit">
+    <div class="row justify-center">
+      <div
+        v-for="credential in credentials"
+        :key="credential.type[1]"
+        class="q-my-none row">
+        <credential-card
+          :credential="credential"
+          :schema="schemaMap[credential.type[1]]" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import {CredentialCard} from 'bedrock-vue-credential-card';
+
+export default {
+  name: 'BrCreateCredentialReview',
+  components: {CredentialCard},
+  props: {
+    subject: {
+      type: String,
+      default: '',
+      required: true
+    },
+    schemaMap: {
+      type: Object,
+      default: () => ({}),
+      required: true
+    },
+    credentials: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  }
+};
+</script>
+<style>
+</style>
--- test/components/Home.vue
+++ test/components/Home.vue
@@ -3,9 +3,14 @@
     class="column gutter-md"
     padding>
     <div class="column items-center">
-      <div>
-        <br-credential-creator-wizard />
-      </div>
+      <br-credential-creator-wizard
+        subject="did:example:subject"
+        issuer="did:example:issuer"
+        :flow="flow"
+        :vocab="vocab"
+        :schema-map="schemaMap"
+        :templates="templates"
+        @finish="finish($event)" />
     </div>
   </q-page>
 </template>
@@ -21,7 +26,106 @@ export default {
   name: 'Home',
   components: {BrCredentialCreatorWizard},
   data() {
-    return {};
+    return {
+      credentials: [],
+      flow: [{
+        icon: {
+          name: 'far fa-list-alt',
+          size: '65px',
+          color: 'primary'
+        },
+        heading: 'Let\'s fill out some information about you',
+        name: 'Personal Information',
+        form: {
+          id: 'user',
+          model: {
+            name: 'Jane Doe',
+            email: 'jane.doe@example.com'
+          },
+          schema: {
+            name: {
+              placeholder: 'Your name'
+            },
+            email: {
+              //component: 'SomeCustomComponent',
+              placeholder: 'Your email address'
+            }
+          }
+        }
+      }/*,
+      {
+        icon: {
+          name: 'fas fa-map-marker-alt',
+          size: '65px',
+          color: 'primary'
+        },
+        heading: 'Where are you located?',
+        name: 'Address Information',
+        form: {
+          id: 'address',
+          model: {
+            address: {}
+          },
+          schema: {
+            address: {
+              component: 'br-address-form'
+            }
+          }
+        }
+      }*/],
+      schemaMap: {
+        PersonCredential: {
+          name: {
+            name: 'Name',
+            icon: 'fas fa-user'
+          },
+          email: {
+            name: 'Email',
+            icon: 'fas fa-envelope'
+          }/*,
+          address: {
+            name: 'Address',
+            icon: 'fa fa-map-marker-alt',
+            sublabels: true
+          }*/
+        }
+      },
+      templates: [`{
+        "@context": [
+          "https://w3id.org/credentials/v1",
+          "https://example.com/examples/v1"
+        ],
+        "type": ["VerifiableCredential", "PersonCredential"],
+        "name": "Personal Details",
+        "description": "Your personal information",
+        "issuer": constants.issuer,
+        "credentialSubject": $merge([
+          {"id": constants.subject},
+          user
+        ])
+      }`],
+      vocab: {
+        name: {
+          range: 'string',
+          label: 'Name',
+          inputType: 'text'
+        },
+        email: {
+          range: 'string',
+          label: 'Email',
+          inputType: 'email'
+        },
+        address: {
+          range: 'PostalAddress',
+          label: 'Address'
+        }
+      }
+    };
+  },
+  methods: {
+    finish({credentials}) {
+      console.log('finish', credentials);
+    }
   }
 };
 </script>
